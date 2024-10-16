b86cf155837190829e300a27f244cfbc33afbc23
Talha Qureshi <talha_qureshi@hotmail.com>
2024-10-16 20:53:25 +0500

    basic react app setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+	return (
+		<>
+			<h1>React + Typescript</h1>
+			<p className="read-the-docs">Use this file as the starting point for all your routes & contexts</p>
+		</>
+	);
+}
+
+export default App;
